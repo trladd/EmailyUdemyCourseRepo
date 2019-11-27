924318775050762bbcfc69f909a3f248382ae77f
trladd <trevarladd@yahoo.com>
2019-11-26 20:45:34 -0500

    Added first part of google oauth that redirects

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express'); //import express from 'express'; would be the way to do it if node.js had es2015 modules (which can be used in react)
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, accessToken => {
+    console.log(accessToken);
+}));
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}))
 
 const PORT = process.env.PORT || 5000; //This is saying use the environment variable from heroku, otherwise use 5000
 app.listen(PORT);
