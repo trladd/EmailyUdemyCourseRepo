a39f8c6275b4954231fc01dca95e9f9e36c94cf7
trladd <trevarladd@yahoo.com>
2019-11-26 20:04:47 -0500

    Started Google OAuth

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 const express = require('express'); //import express from 'express'; would be the way to do it if node.js had es2015 modules (which can be used in react)
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/Dadalyn', (req, res) => {
-    res.send({
-        hi: 'Hi Dadalyn'
-    });
-});
-
-app.get('/', (req, res) => {
-    res.send({
-        hi: 'Hi This is the root'
-    });
-});
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000; //This is saying use the environment variable from heroku, otherwise use 5000
 app.listen(PORT);
