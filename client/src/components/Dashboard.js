57c8845c839179ee7ed25bd28e930671883eab16
trladd <trevarladd@yahoo.com>
2019-12-14 10:04:14 -0500

    Getting started with UI and materialize icons

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Dashboard = () =>{
+    return (
+        <div>
+            Dashboard
+            <div className="fixed-action-btn">
+                <a className="btn-floating btn-large blue">
+                    <i className="material-icons">add</i>
+                </a>
+            </div>
+        </div>
+        
+    );
+};
+
+export default Dashboard;
