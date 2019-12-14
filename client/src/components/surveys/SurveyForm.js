13bbfd666e382337b7e481b55591b1093219e855
trladd <trevarladd@yahoo.com>
2019-12-14 15:45:03 -0500

    Started form work

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,15 @@
+//shows a form for a user to add input
+import React, {Component} from 'react';
+import {reduxForm} from 'redux-form';
+
+class SurveyForm extends Component{
+    render(){
+        return(
+            <div>
+                Survey Form!
+            </div>
+        );
+    }
+}
+
+export default SurveyForm;
