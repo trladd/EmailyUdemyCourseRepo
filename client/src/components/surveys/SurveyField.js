76fe2ee7144bd38bf376e89e77988604a003548a
trladd <trevarladd@yahoo.com>
2019-12-19 10:37:22 -0500

    Getting started with redux form

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,10 @@
+//Survey field contains logic to redner a single label and text input
+import React from 'react';
+
+export default() => {
+    return(
+        <div>
+            <input />
+        </div>
+    );
+};
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,15 +1,31 @@
 //shows a form for a user to add input
 import React, {Component} from 'react';
-import {reduxForm} from 'redux-form';
+import {reduxForm, Field} from 'redux-form';
+import SurveyField from './SurveyField';
 
 class SurveyForm extends Component{
+
+    renderFields(){
+        return(
+            <div>
+                <Field type="text" name="title" component={SurveyField}/>
+            </div>
+        );
+    }
+
     render(){
         return(
             <div>
-                Survey Form!
+                <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+                    {this.renderFields()}
+                    <button type="submit">Submit</button>
+                </form>
+                
             </div>
         );
     }
 }
 
-export default SurveyForm;
+export default reduxForm({
+    form: 'surveyForm'
+})(SurveyForm);
