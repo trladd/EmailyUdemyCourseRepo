ac4c50f68483bae5283a51036f755d1ee3135d87
trladd <trevarladd@yahoo.com>
2020-01-03 13:16:53 -0500

    Added survey new form

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,16 +1,24 @@
 //shows a form for a user to add input
+import _ from 'lodash';
 import React, {Component} from 'react';
 import {reduxForm, Field} from 'redux-form';
+import {Link} from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
+
+const FIELDS = [
+    {label: 'Survey Title', name: 'title'},
+    {label: 'Subject Line', name: 'subject'},
+    {label: 'Email Body', name: 'body'},
+    {label: 'Recipient List', name: 'emails'}
+];
 
 class SurveyForm extends Component{
 
     renderFields(){
-        return(
-            <div>
-                <Field label="Survey Title" type="text" name="title" component={SurveyField}/>
-            </div>
-        );
+        return _.map(FIELDS, ({label,name}) => {
+            return <Field key={name} component={SurveyField} type="text" label={label} name={name}/>
+        })
     }
 
     render(){
@@ -18,7 +26,13 @@ class SurveyForm extends Component{
             <div>
                 <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
                     {this.renderFields()}
-                    <button type="submit">Submit</button>
+                    <Link to="/surveys" className="red btn-flat left white-text">
+                        Cancel
+                    </Link>
+                    <button className="teal btn-flat right white-text" type="submit">
+                        Next
+                        <i className="material-icons right">done</i>
+                    </button>
                 </form>
                 
             </div>
@@ -26,6 +40,32 @@ class SurveyForm extends Component{
     }
 }
 
+function validate(values){
+    const errors={};
+
+    /*if(!values.title){
+        errors.title = 'You must provide a title';
+    }
+    if(!values.subject){
+        errors.subject = 'You must provide a Subject Line';
+    }
+    if(!values.body){
+        errors.body = 'You must provide a Body';
+    } BELOW IS A BETTER WAY TO DO THIS...*/
+    _.each(FIELDS, ({name, label}) => {
+        if(!values[name]){
+            errors[name] = 'You must provide a value for ' + label;
+        }
+    });
+    if(!errors.emails){
+        errors.emails = validateEmails(values.emails || '');
+    }
+ 
+
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: 'surveyForm'
 })(SurveyForm);
