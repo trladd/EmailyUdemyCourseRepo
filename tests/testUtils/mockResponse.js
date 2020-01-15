ecf2f736eab84f071b8b56b6a57c199a767e5158
trladd <trevarladd@yahoo.com>
2020-01-14 20:25:53 -0500

    Added first tests

---
+++ tests/testUtils/mockResponse.js
@@ -0,0 +1,7 @@
+module.exports= () => {
+    const res = {};
+    res.status = jest.fn().mockReturnValue(res);
+    res.json = jest.fn().mockReturnValue(res);
+    res.send = jest.fn().mockReturnValue(res);
+    return res;
+};
