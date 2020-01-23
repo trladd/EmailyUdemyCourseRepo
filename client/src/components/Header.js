469dd4acd93dda14ba7369a483c1e9b877966f0c
trladd <trevarladd@yahoo.com>
2020-01-22 21:54:09 -0500

    Added More advanced login button prepping for more login types

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import M from "materialize-css/dist/js/materialize.min.js";
 
 class Header extends Component {
+    
     componentDidMount() {
         var elem = document.querySelector(".sidenav");
         var instance = M.Sidenav.init(elem, {
@@ -11,6 +12,27 @@ class Header extends Component {
             inDuration: 500, 
             outDuration: 1000
         });
+        const modalOptions = {
+            onOpenStart: () => {
+              console.log("Open Start");
+            },
+            onOpenEnd: () => {
+              console.log("Open End");
+            },
+            onCloseStart: () => {
+              console.log("Close Start");
+            },
+            onCloseEnd: () => {
+              console.log("Close End");
+            },
+            inDuration: 250,
+            outDuration: 250,
+            opacity: 0.5,
+            dismissible: false,
+            startingTop: "4%",
+            endingTop: "10%"
+          };
+          M.Modal.init(this.Modal, modalOptions);
     }
 
     renderAuthContent(headerType) {
@@ -18,7 +40,12 @@ class Header extends Component {
             case null:
                 return;
             case false:
-                return (<li><a href='/auth/google'>Login With Google</a></li>);
+                return (<li><a
+                    className="waves-effect waves-light btn blue modal-trigger"
+                    data-target="modal1"
+                  >
+                    Login
+                  </a></li>);
     
             default:
                 return [
@@ -47,27 +74,64 @@ class Header extends Component {
         console.log(this.props);
         return (
             <div>
-                <nav>
-                    <div className="nav-wrapper deep-orange darken-4 z-depth-3">
-                        <Link 
-                        to={this.props.auth ? '/surveys' : '/'}
-                        className="brand-logo"
-                        >
-                            Emaily
-                        </Link>
-                        <a href="#" data-target="mobileTray" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-                        <ul className="right hide-on-med-and-down">
-                            {this.renderCommonContent("bar")}
-                            {this.renderAuthContent("bar")}
-                        </ul>
-                        
-                    </div>
-                </nav>
-                <ul className="sidenav" id="mobileTray">
-                    {this.renderCommonContent("tray")}
-                    {this.renderAuthContent("tray")}
-                </ul>
+                <div>
+                    <nav>
+                        <div className="nav-wrapper deep-orange darken-4 z-depth-3">
+                            <Link 
+                            to={this.props.auth ? '/surveys' : '/'}
+                            className="brand-logo"
+                            >
+                                Emaily
+                            </Link>
+                            <a href="#" data-target="mobileTray" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                            <ul className="right hide-on-med-and-down">
+                                {this.renderCommonContent("bar")}
+                                {this.renderAuthContent("bar")}
+                            </ul>
+                            
+                        </div>
+                    </nav>
+                    <ul className="sidenav" id="mobileTray">
+                        {this.renderCommonContent("tray")}
+                        {this.renderAuthContent("tray")}
+                    </ul>
+                </div>
+                <div>
+                
+        
+
+        <div
+          ref={Modal => {
+            this.Modal = Modal;
+          }}
+          id="modal1"
+          className="modal"
+        >
+          {/* If you want Bottom Sheet Modal then add 
+        bottom-sheet class */}
+          <div className="modal-content">
+              <ul>
+                  <li>
+                    <a className ="waves-effect btn-flat blue center white-text" href="/auth/google">Login with Google</a>
+                  </li>
+                  <br></br>
+                  <li>
+                    <button className ="waves-effect btn-flat blue center white-text" href="/auth/linkedin">Login with LinkedIn</button>
+                  </li>
+              </ul>
+            
+            
+          </div>
+          <div className="modal-footer">
+            <a href="#" className="modal-close waves-effect waves-white btn-flat">
+              Close
+            </a>
+          </div>
+        </div>
+      
+                </div>
             </div>
+            
            
         )
     }
