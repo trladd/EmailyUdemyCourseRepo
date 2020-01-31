0b2760f6d5d534b99b917a415e12e4773aedebbb
trladd <trevarladd@yahoo.com>
2020-01-31 12:44:25 -0500

    added JSON Editor

--- client/src/components/surveys/directWeb/CreateSurvey.js
+++ client/src/components/surveys/directWeb/CreateSurvey.js
@@ -1,14 +1,22 @@
 import React, {Component} from 'react';
 import queryString from 'query-string'
 import axios from 'axios';
+import JSONInput from 'react-json-editor-ajrm';
+import locale    from 'react-json-editor-ajrm/locale/en';
+
+    
 
 class CreateSurvey extends Component{
     constructor(){
         super();
         this.state={
-            survey: null
+            survey: null,
+            showJSON: false
         };
+        this.updateStateFromEditor = this.updateStateFromEditor.bind(this);
+        this.toggleEditor = this.toggleEditor.bind(this);
     }
+
     async componentDidMount(){
         this.state={
             survey: null
@@ -20,12 +28,57 @@ class CreateSurvey extends Component{
               this.setState({survey: response.data});
         }
         else{
-            this.setState({survey: "put an empty template here"});
+            this.setState({survey: {message: "empty survey"}});
         }
         
     }
+
+    async updateStateFromEditor(newObject){
+        await this.setState({survey: newObject.jsObject});
+    }
+
+    toggleEditor(){
+        console.log(this.state);
+        if(this.state.showJSON){
+            this.setState({showJSON: false});
+        }
+        else{
+            this.setState({showJSON: true});
+        }
+    }
+
+    renderJSONEditor(){
+        if(this.state.showJSON){
+            return(
+                <JSONInput
+                    id          = 'a_unique_id'
+                    placeholder = { this.state.survey }
+                    locale      = { locale }
+                    height      = '90%'
+                    width       = '100%'
+                    onChange = {this.updateStateFromEditor}
+
+                />
+            );
+        }
+    }
+
     render(){
-    return(<div>{JSON.stringify(this.state.survey)}</div>);
+    return(
+        <div>
+            <div>
+                <label>Survey Name</label><br></br>
+                <span>{this.state.survey && this.state.survey.name ? this.state.survey.name : "N/A"}</span>
+            </div>
+            <div>
+                <div>
+                    <button className="waves-effect waves-light btn blue" onClick={this.toggleEditor}>Toggle JSON Editor</button>
+                </div>
+                {this.renderJSONEditor()}
+                
+            </div>
+        </div>
+    );
     }
 }
 
